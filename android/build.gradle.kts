fe1d671f013cf61fd8254b8cf283df36e5176926
somaappteam <somaapp.team@gmail.com>
2026-02-22 19:04:42 +0200

    Fix Android Gradle build: restore v8.10, standardize JVM args and kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,16 +5,13 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.value(rootProject.layout.buildDirectory.dir("../../build").get())
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: Directory = rootProject.layout.buildDirectory.dir(project.name).get()
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
